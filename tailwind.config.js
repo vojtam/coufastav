4a65df75bfa6a770b428ad12963beb454c3aec91
vojtam <macala.vojtech@gmail.com>
2022-08-20 20:14:32 +0200

    index page is halfway done

---
+++ tailwind.config.js
@@ -0,0 +1,58 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    minHeight: {
+      '3/10': '30%',
+      '1/2': '50%',
+      '6/10': '60vh',
+      '7/10': '70%',
+      '8/10': '80%',
+      '1': '100vh',
+    },
+    minWidth: {
+      "3/10": '30%',
+      '6/10': '60%',
+      '7/10': '70%',
+      '1': '100%',
+    },
+    maxWidth: {
+      '7/10': '70%',
+      '8/10': '80%',
+      '9/10': '90%',
+    },
+    maxHeight: {
+      '9/10': '90%'
+    },
+  
+    extend: {
+      fontFamily: {
+        'sans': ['arial', 'sans-serif'],
+      },
+        backgroundImage: {
+          'main-header-img': "url('/img/tile-structure-pattern.jpeg')"
+        },
+      colors: {
+        'overlay-dark': "#272a2e",
+        'vivid-red': "#c20b1d",
+        'darker-red': "#7c1910",
+        'light-gray': "7A7A7A",
+        'dark-green': "#00685a",
+        'border-gray': "#CBD2DE",
+        'light-blue': "#F5F9FD",
+      },
+      boxShadow: {
+        'btn-shadow': "0 16px 32px -16px rgba(255,94,20,.64)",
+        'img-shadow': "64px 64px 160px -40px rgba(0,0,0,.24)",
+      }
+        
+    },
+  },
+  plugins: [],
+}
